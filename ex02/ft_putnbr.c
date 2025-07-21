42f552dab5ebaea07328ea8986b8bf112693b424
0xOZ <97407385+OierZElo@users.noreply.github.com>
2025-07-21 12:55:10 +0000

    Add files via upload

---
+++ ex02/ft_putnbr.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ozugazag <ozugazag@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/21 14:48:36 by ozugazag          #+#    #+#             */
+/*   Updated: 2025/07/21 14:52:53 by ozugazag         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putnbr(int nb)
+{
+	long	n;
+	char	c;
+
+	n = nb;
+	if (n < 0)
+	{
+		c = '-';
+		write(1, &c, 1);
+		n = -n;
+	}
+	if (n >= 10)
+		ft_putnbr(n / 10);
+	c = n % 10 + '0';
+	write(1, &c, 1);
+}
+
+// int	main(void)
+// {
+// 	ft_putnbr(0);
+// 	write(1, "\n", 1);
+// 	ft_putnbr(42);
+// 	write(1, "\n", 1);
+// 	ft_putnbr(-42);
+// 	write(1, "\n", 1);
+// 	ft_putnbr(2147483647); // INT_MAX
+// 	write(1, "\n", 1);
+// 	ft_putnbr(-2147483648); // INT_MIN
+// 	write(1, "\n", 1);
+// 	ft_putnbr(100000);
+// 	write(1, "\n", 1);
+// 	return (0);
+// }
