6f84964401ba9bf951d48039e16d898013656e0d
0xOZ <97407385+OierZElo@users.noreply.github.com>
2025-07-21 12:48:10 +0000

    Add files via upload

---
+++ ex01/ft_putstr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ozugazag <ozugazag@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/21 14:39:43 by ozugazag          #+#    #+#             */
+/*   Updated: 2025/07/21 14:47:14 by ozugazag         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+// int	main(void)
+// {
+// 	ft_putstr("Hola, mundo!\n");
+// 	ft_putstr("Esta es una prueba de ft_putstr.\n");
+// 	ft_putstr("1234567890\n");
+// 	ft_putstr(""); // Cadena vacía, no debería imprimir nada
+// 	ft_putstr("¡Hasta luego!\n");
+// 	return (0);
+// }
